05b70a39befb39551b92efd43f708a356424bceb
Jari Turkia <jatu@hqcodeshop.fi>
2022-02-20 18:51:01 +0200

    Bugfix: There can be much more excluded letters than 10

--- django-api/api/views/serializers/v1/requestv1serializer.py
+++ django-api/api/views/serializers/v1/requestv1serializer.py
@@ -8,5 +8,5 @@ from drf_spectacular.utils import extend_schema_serializer
 )
 class RequestV1Serializer(serializers.Serializer):
     match_mask = serializers.CharField(max_length=10, allow_null=False)
-    excluded_letters = serializers.CharField(max_length=10, allow_null=False)
+    excluded_letters = serializers.CharField(max_length=25, allow_null=False)
     known_letters = serializers.CharField(max_length=10, allow_null=False)
